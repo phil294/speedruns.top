101d2464b469ae4362ed39bea13759dd4f95281b
Philip Waritschlager <philip@waritschlager.de>
2026-08-16 21:57:46 +0200

    fix help icon

--- pages/landing.php
+++ pages/landing.php
@@ -15,7 +15,7 @@ require __DIR__ . '/../templates/header.php';
 ?>
 
 <section id="about">
-	<p>Speedruns.top is an alternative to the popular <a href="https://speedrun.com">speedrun.com</a> site. <? help_icon_html('<figure><blockquote><p>Speedrunning is the act of playing a video game, or section of a video game, with the goal of completing it as fast as possible.</p></blockquote><figcaption><cite><a href="https://en.wikipedia.org/wiki/Speedrunning">en.wikipedia.org</a></cite></figcaption></figure>') ?><br> <cite ></cite>
+	<p>Speedruns.top is an alternative to the popular <a href="https://speedrun.com">speedrun.com</a> site. <? help_icon_html('Speedrunning is the act of playing a video game, or section of a video game, with the goal of completing it as fast as possible. <cite><a href="https://en.wikipedia.org/wiki/Speedrunning">en.wikipedia.org</a></cite>') ?><br>
 	We are <a href="https://github.com/speedruns-top/speedruns.top">open source</a> and accept <em>all</em> sorts of games and categories.</p>
 	<p>Anybody can request the addition of a new game, and you can submit runs to any game. Game moderators will then review and approve them.</p>
 	<p>Feel free to contact us any time via <a href="mailto:contact@speedruns.top">contact@speedruns.top</a>.</p>
@@ -32,7 +32,7 @@ require __DIR__ . '/../templates/header.php';
 	<? foreach ($recent_runs as $run): ?>
 	<tr>
 		<td><?= e(format_date($run['created_at'])) ?></td>
-		<td><a href="/game/<?= e($run['name']) ?>"><?= e($run['game_name']) ?></a> / <?= e($run['category_name']) ?></td>
+		<td><a href="/game/<?= e($run['game_name']) ?>"><?= e($run['game_name']) ?></a> / <?= e($run['category_name']) ?></td>
 		<td><a href="/user/<?= e($run['user_name']) ?>"><?= e($run['user_name']) ?></a></td>
 		<td><?= e(format_run_time((int) $run['time_milliseconds'])) ?></td>
 		<td><span class="tag"><?= $run['verified'] === null ? 'pending' : 'verified' ?></span></td>
